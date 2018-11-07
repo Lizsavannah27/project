bdeab9d3e959f8e380c5d2cab45a8fa8109df281
MercyJebiwot <44652171+MercyJebiwot@users.noreply.github.com>
2018-11-07 11:43:25 +0300

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME', 'e-commerce');
+$conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//echo "Connected successfully";
+?>
